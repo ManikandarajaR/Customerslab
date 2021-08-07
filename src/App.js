cc2205607bf82763d94c6df2b7d5cc7407b429c4
manikandarajar.20mca@kongu.edu <manikandanraja1204@gmail.com>
2021-08-07 15:51:06 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Navbar from './components/navbar';
+import Button from './components/button';
+
 
 function App() {
   return (
     <div className="App">
-          <h1>Manikandaraja</h1>
-          <p>The American college....</p>
-          <h2>Kongu engineering college..</h2>
-          <h3>Muthu pandi</h3>
-          <h3> Mani</h3>
+
+    <Navbar/>
+    <Button/>
     </div>
   );
 }
