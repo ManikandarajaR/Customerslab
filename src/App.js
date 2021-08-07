0d74b6dbf0585d8c7306bb163716467ba70ee90e
manikandarajar.20mca@kongu.edu <manikandanraja1204@gmail.com>
2021-08-07 11:03:10 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
           <p>The American college....</p>
           <h2>Kongu engineering college..</h2>
           <h3>Muthu pandi</h3>
+          <h3> Mani</h3>
     </div>
   );
 }
